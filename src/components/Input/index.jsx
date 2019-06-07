26ae933d2d19d09430f88d971b1e169f1b31c375
Jusley Arley <jusleyarley@hotmail.com>
2019-06-06 23:44:25 -0300

    Mudança para materialize, alteração no login para o materialize

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
+// import "./css/style.css";
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+function InputWithIcon(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.margin}>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <img src={props.img} alt=""/>
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid"
+              label={props.label}
+              name={props.name}
+              onChange={props.onChange}
+              type={props.type}
+              value={props.value}
+            />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+export default InputWithIcon;
--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import { NavLink, Redirect } from "react-router-dom";
 import SimpleReactValidator from "simple-react-validator";
-import auth from "../../auth";
+// import { NavLink, Redirect } from "react-router-dom";
 import withConsumer from "../../components/AppContext/withConsumer";
+// import auth from "../../auth";
 
-import Button from "../../components/Button";
-// import Input from "../../components/Input";
+// import Button from "../../components/Button";
+import Input from "../../components/Input";
 
+import email from "./img/email.svg";
 import logo from "./img/logo.png";
+// import senha from "./img/senha.svg";
 import "./css/style.css";
 
 class Login extends Component {
@@ -38,51 +40,7 @@ class Login extends Component {
   };
 
   handleSubmit = async e => {
-    try {
-      e.preventDefault();
-
-      this.setState({ isLoadingButton: true });
-
-      this.setState({ answer: "" });
-
-      if (!this.validator.allValid()) {
-        this.setState({
-          answer: [
-            <i className="material-icons" key="">
-              error_outline
-            </i>,
-            "Erros de validações nos campos."
-          ]
-        });
-        this.setState({ isLoadingButton: false });
-        this.validator.showMessages();
-        this.forceUpdate();
-        return;
-      }
-
-      await auth.login(this.state.email, this.state.password);
-    } catch (error) {
-      if (error.code === "auth/user-disabled") {
-        this.setState({
-          answer: [
-            <i className="material-icons align-icon" key="">
-              error_outline
-            </i>,
-            "Usuário desabilitado. Entre em contato com o administrador!"
-          ]
-        });
-      } else {
-        this.setState({
-          answer: [
-            <i className="material-icons align-icon" key="">
-              error_outline
-            </i>,
-            "E-mail ou senha inválidos. Tente novamente!"
-          ]
-        });
-      }
-      this.setState({ isLoadingButton: false });
-    }
+    e.preventDefault();
   };
 
   render() {
@@ -90,77 +48,60 @@ class Login extends Component {
       <div id="login-view">
         <section>
           <div className="container">
-            <div className="logo">
+            <div className="row">
+              <div className="col s12 m12 l12 xl12">
+                <img src={logo} alt=""/>
+              </div>
+            </div>
+            
+            <div className="row">
+              <div className="col s12 m12 l12 xl12 center-align">
+                <p><b>BEM-VINDO AO EMPRESAS</b></p>
+                <p>Lorem ipsum dolor sit amet, contetur<br /> adipiscing elit. Nunc accumsan.</p>
+              </div>
+            </div>
+
+            <form className="col s12">
               <div className="row">
-                <div className="col-sm">
-                  <img src={logo} alt="" />
+                <div class="input-field col s6">
+                  {/* <img src={email} alt=""/> */}
+                  <input id="icon_prefix" type="text" class="validate" />
+                  <label for="icon_prefix">E-mail</label>
                 </div>
               </div>
-            </div>
-
-            <div className="row">
-              <div className="col">
-
-              </div>
-            </div>
-{/* 
-            <div className="columns is-centered">
-              <div className="column is-8-tablet is-6-desktop">
-                <div className="box-round">
-                  <form onSubmit={this.handleSubmit}>
-                    <div className="field"> */}
-                      {/* <Input
-                        label="Login"
-                        name="email"
-                        type="email"
-                        value={this.state.email}
-                        onChange={this.handleFormChange}
-                      /> */}
-                      {/* <span>
-                        {this.validator.message(
-                          "email",
-                          this.state.email,
-                          "required"
-                        )}
-                      </span> */}
-                    {/* </div>
-
-                    <div className="field margin-input"> */}
-                      {/* <Input
-                        label="Senha"
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.handleFormChange}
-                      />
-                      <span>
-                        {this.validator.message(
-                          "password",
-                          this.state.password,
-                          "required"
-                        )}
-                      </span> */}
-                    {/* </div>
-
-                    <div className="answer margin-input">
-                      {this.state.answer}
-                    </div> */}
-
-                    <div className="field margin-other btn">
-                      <Button name="Entrar" type="submit" isLoading={this.state.isLoadingButton} />
-                    </div>
-
-                    {/* <div className="field margin-other forgot">
-                      <NavLink to="/login/redefinir-senha">
-                        Esqueceu sua senha?
-                      </NavLink>
-                    </div> */}
-                  {/* </form> */}
-                {/* </div> */}
-              {/* </div> */}
-            {/* </div> */}
+            </form>
           </div>
         </section>
+        {/* <section> */}
+
+            {/*
+                <form>
+                  <div className="row">
+                    <div className="col-sm-4 col-1" />
+                    <div className="col-sm-4 center justify-content-center">
+                      <Input label="E-mail" img={email} name="email" type="email" value={this.state.email} onChange={this.handleFormChange} />
+                    </div>
+                    <div className="col-sm-4" />
+                  </div>
+                  <div className="row">
+                    <div className="col-sm-4" />
+                    <div className="col-sm-4 center justify-content-center">
+                      <Input label="E-mail" img={senha} name="email" type="email" value={this.state.email} onChange={this.handleFormChange} />
+                    </div>
+                    <div className="col-sm-4" />
+                  </div>
+                  <div className="row margin">
+                    <div className="col-sm-4" />
+                    <div className="col-sm-4 center justify-content-center">
+                      <Button name="Entrar" type="submit" isLoading={this.state.isLoadingButton} />
+                    </div>
+                    <div className="col-sm-4" />
+                  </div>
+                </form>
+              </div>
+            </div> */}
+          {/* </div> */}
+        {/* </section> */}
       </div>
     );
   }
